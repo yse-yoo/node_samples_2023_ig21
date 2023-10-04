e61a208855ad6dee24abb434e1ec60e07a72a00b
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-04 11:33:27 +0900

    load config

--- express_sample/server.js
+++ express_sample/server.js
@@ -2,3 +2,11 @@
 const express = require('express')
 // dotenvモジュール読み込み
 const dotenv = require('dotenv')
+
+// dotenvの設定読み込み
+dotenv.config()
+const HOST = process.env.HOST
+const PORT = process.env.PORT
+
+console.log(HOST)
+console.log(PORT)
