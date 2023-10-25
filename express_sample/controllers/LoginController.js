4273d48b6ad4e9dcf7826abc7d30ca6a028bd68f
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-25 12:07:06 +0900

    add LoginController

---
+++ express_sample/controllers/LoginController.js
@@ -0,0 +1,6 @@
+
+// 入力画面（TOP)
+exports.index = (req, res) => {
+    // views/loign/index.ejs を表示
+    res.render('login/index')
+}
--- express_sample/routes.js
+++ express_sample/routes.js
@@ -3,9 +3,10 @@ const express = require('express')
 // Routerオブジェクトを生成
 const router = express.Router()
 
-// HomeControllerモジュール読み込み
+// Controllerモジュール読み込み
 const HomeController = require('./controllers/HomeController')
 const ItemController = require('./controllers/ItemController')
+const LoginController = require('./controllers/LoginController')
 
 // GETリクエストの処理
 // Home
@@ -15,6 +16,9 @@ router.get('/profile', HomeController.profile)
 // Item
 router.get('/item', ItemController.index)
 router.get('/item/:id', ItemController.detail)
+
+// Login
+router.get('/login', LoginController.index)
 
 // POSTリクエスト
 router.post('/auth', (req, res) => {
