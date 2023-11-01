8380e7cf33ac7a950d92c8cb6b3c876818f5d5c2
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-01 09:37:44 +0900

    add Model

--- express_sample/models/Model.js
+++ express_sample/models/Model.js
@@ -1,5 +1,29 @@
+// fs モジュールを読み込み
+const fs = require('fs')
 class Model {
+    dataFile = ""
 
+    /**
+     * すべてのデータを取得
+     * @returns array
+     */
+    get = () => {
+        //外部ファイルの読み込み
+        var json = fs.readFileSync(this.dataFile)
+        // JSONデータをパース（オブジェクトに変換）
+        var values = JSON.parse(json);
+        return values;
+    }
+    
+    /**
+     * IDを指定してデータ取得するメソッド
+     * @returns array
+     */
+    find = (id) => {
+        var values = this.get();
+        //データを繰り返して、id が一致したら返す
+        return values.find((value) => value.id == id);
+    }
 }
 
 //モジュール化
