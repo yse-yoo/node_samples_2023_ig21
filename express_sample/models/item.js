09fd996e82a6fab138d6c54be3e996c9d4b312bf
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-01 09:44:49 +0900

    update

--- express_sample/models/item.js
+++ express_sample/models/item.js
@@ -1,21 +1,7 @@
-// fs モジュールを読み込み
-const fs = require('fs')
+//Modelモジュール読み込み
+const Model = require('./Model')
 
-// data/items.jsonのパス設定
-exports.filePath = "./data/items.json"
-
-// すべてのデータを取得するメソッド
-exports.get = () => {
-    //外部ファイルの読み込み
-    var json = fs.readFileSync(this.filePath)
-    // JSONデータをパース（オブジェクトに変換）
-    var values = JSON.parse(json);
-    return values;
-}
-
-// IDを指定してデータ取得するメソッド
-exports.find = (id) => {
-    var values = this.get();
-    //データを繰り返して、id が一致したら返す
-    return values.find((value) => value.id == id);
+//Modelクラスを継承
+class Item extends Model {
+    dataFile = "./data/items.json"
 }
