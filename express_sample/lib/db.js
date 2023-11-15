5b9d7ab11eb797123ec423689477c1eebeb6e95e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-15 10:48:22 +0900

    update

---
+++ express_sample/lib/db.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+exports.info = {
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USESR,
+    password: process.env.DB_PASSWORD,
+}
