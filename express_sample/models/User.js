06ced707b0df05294fa6cbd4ac89c2501f35742a
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-15 11:26:47 +0900

    update

--- express_sample/models/User.js
+++ express_sample/models/User.js
@@ -5,7 +5,19 @@ const db = require('../lib/db')
 class User {
     //ユーザ追加（非同期処理）
     add = async (post) => {
-
+        //SQL
+        var sql = `INSERT INTO users SET ?;`
+        //DB接続
+        //SQL実行
+        try {
+            const con = await mysql.createConnection(db.info);
+            var reulst;
+            result = con.query(sql, post);
+        } catch (error) {
+            
+        } finally {
+            con.end();
+        }
     }
     auth = (email, password) => {
 
